dc5315aff67ff7d6551c41dd53d6053f644df9ef
Jens Deprez <jens.deprez@gmail.com>
2026-02-24 22:30:17 +0100

    Update: add hardcoded response trigger

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+#Define a new Gemini Client
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+
+print(response.text)
+
+if api_key == None:
+    raise RuntimeError("No valid API key detected")
+
 def main():
     print("Hello from ai-agent!")
 
