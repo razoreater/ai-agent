9a29d5cd071fabb2003110a9cb2d4f6c5e67c2ee
Jens Deprez <jens.deprez@gmail.com>
2026-02-27 20:23:52 +0100

    Update: added optional verbose output

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
     #This makes it so that we can access "args.user_prompt"
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     #Defining a conversation history
@@ -28,9 +29,13 @@ def main():
     if response.usage_metadata is None:
         raise RuntimeError("No metadata receives, invalid API request?")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt} ")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
