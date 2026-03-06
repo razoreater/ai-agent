ba1a276339907069871f18bfa05acef688010bcc
Jens Deprez <jens.deprez@gmail.com>
2026-03-06 21:26:34 +0100

    Update: added the file content function This function reads the defined number of characters of a file and the gives them as output

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,23 @@
+import os
+from config import MAX_CHARS
+
 def get_file_content(working_directory, file_path):
-    
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+    common_path = os.path.commonpath([abs_working_dir, abs_file_path])
+
+    try:
+        if common_path != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        #Open the file an read the first max characters from imported file
+        with open(abs_file_path, "r") as file:
+            data = file.read(MAX_CHARS)
+            if file.read(1):
+                data += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return data
+    except Exception as error:
+        return f"Error: {error}"
+
