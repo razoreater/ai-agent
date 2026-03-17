909e2b4d6e44048f97e1737c548e87e5890e1d2f
Jens Deprez <jens.deprez@gmail.com>
2026-03-17 21:50:14 +0100

    Update: First function declaration for the Agent

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, argparse
 from dotenv import load_dotenv
 from google import genai
 from prompts import system_prompt
+from functions.call_function import available_functions
 
 def main():
     load_dotenv()
@@ -26,7 +27,10 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=genai.types.GenerateContentConfig(system_instruction=system_prompt),
+        config=genai.types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            tools=[available_functions],
+            ),
     )
 
     if response.usage_metadata is None:
@@ -36,9 +40,17 @@ def main():
         print(f"User prompt: {args.user_prompt} ")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(response.text)
+        if response.function_calls:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(response.text)
     else:
-        print(response.text)
+        if response.function_calls:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(response.text)
 
 if __name__ == "__main__":
     main()
